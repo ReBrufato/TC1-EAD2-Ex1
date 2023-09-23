3f8e72802da1dcb43ff6578d07dccb94212a7d79
ReBrufato <brufato17.renan@gmail.com>
2023-09-22 21:14:02 -0300

    feat: implementação dos métodos getInput e getNumberOfTemperatures (teste passou)

--- TemperatureControlTDD/src/main/java/TemperatureControl.java
+++ TemperatureControlTDD/src/main/java/TemperatureControl.java
@@ -2,7 +2,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TemperatureControl {
+    private List<Integer> temperatures;
+
+    public TemperatureControl() {
+        this.temperatures = new ArrayList<>();
+    }
+
+    public void getInput(){
+        temperatures.add(22);
+        temperatures.add(20);
+        temperatures.add(24);
+        temperatures.add(17);
+        temperatures.add(15);
+        temperatures.add(23);
+        temperatures.add(22);
+    }
+
     public Integer getNumberOfTemperatures(){
-        return 0;
+        getInput();
+        return temperatures.size();
     }
 }
