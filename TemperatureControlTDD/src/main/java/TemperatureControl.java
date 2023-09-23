75b1eb94660f5a86269ba987aafa06a9dbeb4b1b
ReBrufato <brufato17.renan@gmail.com>
2023-09-22 21:09:58 -0300

    test: teste theUserMustEnterSevenTemperatureValues criado

--- TemperatureControlTDD/src/main/java/TemperatureControl.java
+++ TemperatureControlTDD/src/main/java/TemperatureControl.java
@@ -1,2 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TemperatureControl {
+    public Integer getNumberOfTemperatures(){
+        return 0;
+    }
 }
--- TemperatureControlTDD/src/test/java/TemperatureControlTest.java
+++ TemperatureControlTDD/src/test/java/TemperatureControlTest.java
@@ -1,5 +1,15 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TemperatureControlTest {
-
+    @Test
+    @DisplayName("theUserMustEnterSevenTemperatureValues")
+    void theUserMustEnterSevenTemperatureValues() {
+        TemperatureControl sut = new TemperatureControl();
+        Integer numberInputs = sut.getNumberOfTemperatures();
+        assertThat(numberInputs).isEqualTo(7);
+    }
 }
